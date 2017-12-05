8efc33890ac31d41169576adcdfd2a9c670a82f2
Peter Vandervelde <pvandervelde@Peters-Rasberry-Pi-air.local>
2017-12-05 12:13:44 -0800

    testing results

--- repeat.py
+++ repeat.py
@@ -1,18 +1,18 @@
-from controller import names, Controller, vals
+from controller import names, Controller
 
 vals = []
-#for i in range(28, 34):
-#    for j in range(28, 34):
-#        for k in range(28, 34):
-#            vals.append([i, j, k])
-# can be used for more in-deapth testing
-for i in range(20, 35):
-    vals.append([i, i, i])
+for i in range(28, 34):
+    for j in range(28, 34):
+        for k in range(28, 34):
+            vals.append([i, j, k])
+#can be used for more in-deapth testing
+#for i in range(20, 35):
+#    vals.append([i, i, i])
 
 
 scoresums = [0] * len(vals)
-for _ in range(1000):
-    game = Controller()
+for _ in range(1000000):
+    game = Controller(vals)
     while any(x >= 0 for x in game.funds):
         game.set_distances()
         game.gather_bids()
@@ -23,7 +23,7 @@ for _ in range(1000):
     for j in range(5):
         scoresums[game.players[j]] += game.teamscores[j]
 
-print("Sum of scores for all teams tested 1000 times:")
+print("Sum of scores for all teams tested 100000 times:")
 
 ordered = sorted(zip(scoresums, range(len(vals))), reverse=True)
 for score, j in ordered:
